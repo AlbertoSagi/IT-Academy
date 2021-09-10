6e2fc9e4361dfd2fd17c741be370f1c7a4aa7161
Alberto <alberto.sagi@gmail.com>
2021-09-10 17:12:48 +0200

    M02_N1_F1

---
+++ M02_tasca/M02N1F1/src/M02N1F1App.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class M02N1F1App {
+
+	public static void main(String[] args) {
+		
+		String nom = JOptionPane.showInputDialog("Dime tu nombre");
+		char char_nom[] = nom.toCharArray();
+		
+		for (int i=0; i<char_nom.length; i++) {
+			System.out.println(char_nom[i]);
+		}
+
+	}
+}
